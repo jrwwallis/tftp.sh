4d4eff77009ef477e9147227bfbc5eb12e566ca8
John Wallis <jwallis@cisco.com>
2019-12-31 08:21:02 -0800

    tftp-get.sh header

---
+++ tftp-get.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+###############################################################################
+#
+# Bash-based implementation of the TFTP protocol - GET client
+# John Wallis 2020
+# GPLv3
+# https://github.com/jrwwallis/tftp.sh
+#
+###############################################################################
